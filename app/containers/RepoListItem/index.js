4697b31dc5f97d52df69c2420682d3f6f0a14e4c
Michal Kopulety <michal.kopulety@sap.com>
2018-10-07 19:28:24 +0200

    Update start script

--- app/containers/RepoListItem/index.js
+++ app/containers/RepoListItem/index.js
@@ -1,10 +1,8 @@
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { makeSelectCurrentUser } from 'containers/App/selectors';
 import RepoListItem from './RepoListItem';
 
 export default connect(
   createStructuredSelector({
-    currentUser: makeSelectCurrentUser()
   })
 )(RepoListItem);

7f4044caf531817beeb365b463922ceda9a6303e
Michal Kopulety <michal.kopulety@sap.com>
2018-10-07 19:44:30 +0200

    Update start script

--- config/webpack.base.babel.js
+++ config/webpack.base.babel.js
@@ -25,18 +25,6 @@ module.exports = (options) => ({
         },
       },
       {
-        // Preprocess our own .scss files
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        // Preprocess 3rd party .css files located in node_modules
-        test: /\.css$/,
-        include: /node_modules/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
         test: /\.(eot|svg|otf|ttf|woff|woff2)$/,
         use: 'file-loader',
       },
